88065d7be4a88f47cce1439e0dd0c5605d0a5867
Furqan Ahmed <98402746+furqanahmed1872@users.noreply.github.com>
2024-09-12 22:14:01 +0500

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+ import { Button } from "$lib/components/ui/button";
+</script>
+ 
+<div
+class="bg-slate-900 w-full h-screen grid place-content-center text-2xl text-white"
+>
+<Button>Click me</Button>
+  Pages render here
+</div>

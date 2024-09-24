e93b12f185bc4203edfe44b3edb47615178323bc
Your Name <abdullaharifsalimee@gmail.com>
2024-09-24 19:48:55 +0500

    ok

--- src/routes/(app)/test/+page.svelte
+++ src/routes/(app)/test/+page.svelte
@@ -1,25 +1,206 @@
-<script>
-  // Target the jaw element
-  let jaw = document.getElementById('jaw');
+<div class="waveContainer">
+  <div class="wave wave1"></div>
+  <div class="wave wave2"></div>
+  <div class="wave wave3"></div>
+  <div class="wave wave4"></div>
+  <div class="wave wave5"></div> 
+  <div class="wave wave2"></div>
+  <div class="wave wave4"></div>
 
-  // Animate jaw during speech
-  function moveJaw() {
-    jaw.style.transform = 'translateY(10px)';
-    setTimeout(() => {
-      jaw.style.transform = 'translateY(0)';
-    }, 100); // Adjust timing to match speaking
-  }
+</div>
 
-  // Simulate chatbot speaking event
-  document.querySelector('#Buddy').addEventListener('click', function() {
-    moveJaw();  // Trigger the jaw movement on click for testing
-  });
+<style>
 
-  // If you want to trigger it when the chatbot speaks:
-  // chatbot.on('speak', function() {
-  //   moveJaw();
-  // });
-</script>
+body {
+background: #222;
+}
 
-<img src="/buddy_c.svg" width="500px" alt="">
+.waveContainer {
+position: fixed;
+left: 0;
+right: 0;
+top: 50%;
+transform: translate3d(0, -50%, 0);
+-webkit-transform: translate3d(0, -50%, 0);
+-moz-transform: translate3d(0, -50%, 0);
+margin: 0 auto;
+display: flex;
+justify-content: space-between;
+height: 68px;
+--boxSize: 9px;
+--gutter: 4px;
+width: calc((var(--boxSize) + var(--gutter)) * 7);
+align-items: center;
+}
 
+.wave {
+-webkit-transform: scaleY(1);
+transform: scaleY(1);
+height: 50%;
+width: var(--boxSize);
+background: #fff; /* Make all waves white */
+-webkit-animation-duration: 0.8s;
+animation-duration: 0.8s;
+-webkit-animation-timing-function: ease-in-out;
+animation-timing-function: ease-in-out;
+-webkit-animation-iteration-count: infinite;
+animation-iteration-count: infinite;
+border-radius: 80px;
+}
+
+.wave1 {
+-webkit-animation-name: random1;
+animation-name: random1;
+}
+
+.wave2 {
+-webkit-animation-name: random2;
+animation-name: random2;
+-webkit-animation-delay: 0.2s;
+animation-delay: 0.2s;
+}
+
+.wave3 {
+-webkit-animation-name: random3;
+animation-name: random3;
+-webkit-animation-delay: 0.3s;
+animation-delay: 0.3s;
+}
+
+.wave4 {
+-webkit-animation-name: random4;
+animation-name: random4;
+-webkit-animation-delay: 0.4s;
+animation-delay: 0.4s;
+}
+
+.wave5 {
+-webkit-animation-name: random5;
+animation-name: random5;
+-webkit-animation-delay: 0.2s;
+animation-delay: 0.2s;
+}
+
+@-webkit-keyframes random1 {
+0% {
+  height: 25%;
+}
+50% {
+  height: 100%;
+}
+100% {
+  height: 25%;
+}
+}
+
+@keyframes random1 {
+0% {
+  height: 25%;
+}
+50% {
+  height: 100%;
+}
+100% {
+  height: 25%;
+}
+}
+
+@-webkit-keyframes random2 {
+0% {
+  height: 22%;
+}
+50% {
+  height: 65%;
+}
+100% {
+  height: 22%;
+}
+}
+
+@keyframes random2 {
+0% {
+  height: 22%;
+}
+50% {
+  height: 65%;
+}
+100% {
+  height: 22%;
+}
+}
+
+@-webkit-keyframes random3 {
+0% {
+  height: 65%;
+}
+50% {
+  height: 20%;
+}
+100% {
+  height: 65%;
+}
+}
+
+@keyframes random3 {
+0% {
+  height: 65%;
+}
+50% {
+  height: 20%;
+}
+100% {
+  height: 65%;
+}
+}
+
+@-webkit-keyframes random4 {
+0% {
+  height: 20%;
+}
+50% {
+  height: 80%;
+}
+100% {
+  height: 20%;
+}
+}
+
+@keyframes random4 {
+0% {
+  height: 20%;
+}
+50% {
+  height: 80%;
+}
+100% {
+  height: 20%;
+}
+}
+
+@-webkit-keyframes random5 {
+0% {
+  height: 100%;
+}
+50% {
+  height: 35%;
+}
+100% {
+  height: 100%;
+}
+}
+
+@keyframes random5 {
+0% {
+  height: 100%;
+}
+50% {
+  height: 35%;
+}
+100% {
+  height: 100%;
+}
+}
+
+
+
+</style>

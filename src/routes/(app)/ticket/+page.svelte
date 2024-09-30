1bf49f1bdf5fafa47b142b153f2ba3ded86f88cd
Your Name <abdullaharifsalimee@gmail.com>
2024-10-01 00:11:55 +0500

    ok

--- src/routes/(app)/ticket/+page.svelte
+++ src/routes/(app)/ticket/+page.svelte
@@ -1,66 +1,70 @@
-<div class="grid h-fit w-1/3 rounded-xl border bg-primary m-auto">
-  <div class="text-secondary text-2xl font-normal text-center p-4">
-    Create Agent
-  </div>
-  <hr class="border-0 h-3 bg-secondary my-2" />
+<div class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70">
+  <div class="bg-primary rounded-lg w-fit ">
+    <div class="text-white text-xl font-semibold p-4">Create Agent</div>
+    <hr class="border-t-8 border-secondary " />
 
-  <div class="flex m-2 items-center">
-    <div class="m-2 w-full items-center">
-      <label class="text-secondary text-lg " for="name">Name</label>
-      <input
-        id="name"
-        class="w-full rounded-sm p-1 bg-lightBlue my-1"
-        type="text"
-        required
-      />
-      <div class="w-full relative">
-        <label class="text-secondary text-lg " for="name">Voice</label>
-        <input
-          type="text"
-          id="readonly-input"
-          value=" Avatar Default"
-          readonly
-          class="w-full bg-lightBlue rounded-sm my-1 text-secondary p-1 text-2xl font-light outline-none"
-          placeholder=""
-        />
-        <button class="absolute inset-y-8 right-0  px-4 py-2">
-          <img src="./play.png" class="w-6 h-6" />
-        </button>
-      </div>
-    </div>
 
-    <img
-      src="./person.jpg"
-      class="rounded-lg border border-mediumBlue w-40 h-40 mx-2"
-      alt=""
-    />
-  </div >
-  <div class="px-4">
-    <label class="text-secondary text-lg " for="name">Visibility</label>
-    <select name="Public" id="visibility" class="w-full bg-lightBlue rounded-sm my-1 text-white p-1 text-2xl font-light outline-none">
+    <div class="grid grid-cols-10 items-center p-2">
+      <div class="col-span-7 gap-2 mx-2">
+        <div class="grid grid-cols-7 items-center py-2">
+          <label for="" class="text-secondary col-span-3 text-lg font-light">Name</label>
+          <input type="text" class="col-span-4 text-lg font-light text-secondary h-10 w-full bg-lightBlue rounded leading-tight" />
+        </div>
+
+        <div class="grid grid-cols-7 items-center py-2">
+          <label for="" class="text-secondary col-span-3 text-lg font-light">Voice</label>
+          <select class="text-lg col-span-4 font-light text-secondary h-10 w-full bg-lightBlue rounded leading-tight" id="grid-state">
+            <option>Avatar Default</option>
+           
+          </select>
+        </div>
+
+        <div class="grid grid-cols-7 items-center py-2">
+          <label for="" class="text-secondary col-span-3 text-lg font-light">Visibility</label>
+          <select class="text-lg col-span-4 font-light text-secondary h-10 w-full bg-lightBlue rounded leading-tight" id="grid-state">
+            <option>Public</option>
+            <option>Private</option>
       
-      <option value="Public">Public</option>
-      <option value="Private">Private</option>
-    </select>
- </div>
- <div class="justify-center text-center m-4">
- <label class="text-secondary text-lg text-center" for="name">Description</label>
-      <input
-        id="name"
-        placeholder="In just few words,how would you describe this agent ?"
-        class="w-full rounded-sm p-3 bg-mediumBlue my-1 overflow-scroll"
-        type="text"
-        required
-      />
-  
-      <label class="text-secondary text-lg text-center" for="name">Instruction</label>
-      <input
-        id="name"
-        placeholder="Optional: Elaborate on the role if needed"
-        class="w-full rounded-sm p-3 bg-mediumtBlue my-1 h-36"
-        type="text"
-        required
-      />
-    </div>
+          </select>
+        </div>
+      </div>
 
+      <img src="./person.jpg" alt="" class="w-40 h-40 rounded-lg m-2 col-span-3 " />
+    
+
+    
+    </div>
+  
+    <!-- <div class="grid grid-cols-10 items-center  mx-2">
+      <label for="" class="text-secondary col-span-3 text-lg font-light mx-2">Action</label>
+      <select class="text-lg col-span-7 font-light text-secondary mr-2 h-10 w-full bg-lightBlue rounded leading-tight" id="grid-state">
+        <option>New Mexico</option>
+        <option>Missouri</option>
+        <option>Texas</option>
+      </select>
+    </div>
+   -->
+  
+   <div class="grid grid-cols-10 items-center mx-2">
+    <label for="description1" class="text-secondary col-span-3 text-lg font-light mx-2">Description</label>
+    <textarea 
+      id="description1"
+      rows="2"
+      placeholder="In just a few words, how would you describe this agent?"
+      class="text-lg col-span-7 font-light text-secondary mr-2 h-20 w-full bg-lightBlue rounded leading-tight  ">
+    </textarea>
+  </div>
+  
+  <div class="grid grid-cols-10 items-center my-4 mx-2">
+    <label for="description2" class="text-secondary col-span-3 text-lg font-light mx-2">Instruction</label>
+    <textarea 
+      id="description2"
+      rows="7"
+      placeholder="In just a few words, how would you describe this agent?"
+      class="text-lg col-span-7 font-light text-secondary mr-2 h-40 w-full bg-lightBlue rounded leading-tight">
+    </textarea>
+  </div>
+  
+
+  </div>
 </div>

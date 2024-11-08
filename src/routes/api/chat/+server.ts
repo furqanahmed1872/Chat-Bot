b3538e7b4b8396ee408281bdcdbcb412e9e8535f
Furqan Ahmed <98402746+furqanahmed1872@users.noreply.github.com>
2024-11-08 21:18:08 +0500

    ok

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,10 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
 import { json } from '@sveltejs/kit';
 import fs from 'fs';
 import path from 'path';
 import OpenAI from 'openai';
 import { PRIVATE_OPENAI_API_KEY } from '$env/static/private';
+import { PRIVATE_SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+
 export async function POST({ request }) {
-  console.log(PRIVATE_OPENAI_API_KEY);
+  const supabase = createClient(
+    PUBLIC_SUPABASE_URL,
+    PRIVATE_SUPABASE_SERVICE_ROLE_KEY,
+  );
   const { message, voice, prompt } = await request.json();
   const openai = new OpenAI({ apiKey: PRIVATE_OPENAI_API_KEY });
 
@@ -41,33 +48,52 @@ export async function POST({ request }) {
       }),
     });
     // for voice chat reply
-    // const data = await response.json();
-    // if (data?.choices?.length > 0) {
-    //   const aiResponseText = data.choices[0].message.content;
+    const data = await response.json();
+    if (data?.choices?.length > 0) {
+      const aiResponseText = data.choices[0].message.content;
 
-    //   const speechFile = path.resolve('./static/speech.mp3');
+      const mp3 = await openai.audio.speech.create({
+        model: 'tts-1',
+        voice: voice ? voice : 'alloy',
+        input: aiResponseText,
+      });
+      const buffer = Buffer.from(await mp3.arrayBuffer());
 
-    //   const mp3 = await openai.audio.speech.create({
-    //     model: 'tts-1',
-    //     voice: voice ? voice : 'alloy',
-    //     input: aiResponseText,
-    //   });
+      const { data: fileList, error: listError } = await supabase.storage
+        .from('audio-bucket')
+        .list('', { search: 'speech.mp3' });
 
-    //   const buffer = Buffer.from(await mp3.arrayBuffer());
+      if (fileList) {
+        const { error: deleteError } = await supabase.storage
+          .from('audio-bucket')
+          .remove(['speech.mp3']);
 
-    //   await fs.promises.writeFile(speechFile, buffer);
+        const { data: audio, error } = await supabase.storage
+          .from('audio-bucket')
+          .upload('speech.mp3', buffer, {
+            contentType: 'audio/mp3',
+          });
 
-    //   return json({
-    //     message: aiResponseText,
-    //     audio: `/speech.mp3?t=${Date.now()}`,
-    //   });
-    // } else {
-    //   console.error('No choices found in the API response');
-    //   return json(
-    //     { message: 'Error: No response from the AI' },
-    //     { status: 500 },
-    //   );
-    // }
+        if (error) {
+          console.error('Error uploading file:', error);
+          return json(
+            { message: 'Error uploading audio file' },
+            { status: 500 },
+          );
+        }
+
+        return json({
+          message: aiResponseText,
+          audio: `https://abgxvtnbxnynzmfbriqk.supabase.co/storage/v1/object/public/audio-bucket/speech.mp3`,
+        });
+      } else {
+        console.error('No choices found in the API response');
+        return json(
+          { message: 'Error: No response from the AI' },
+          { status: 500 },
+        );
+      }
+    }
   } catch (error) {
     console.error('Error fetching from OpenAI:', error);
     return json({ message: 'Error fetching from AI' }, { status: 500 });

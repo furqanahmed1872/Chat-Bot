08772f87e7e2e10ab016502686c860dd9fa928ee
Furqan Ahmed <98402746+furqanahmed1872@users.noreply.github.com>
2024-11-08 14:21:56 +0500

    ok

--- src/routes/(auth)/forget-password/+page.server.ts
+++ src/routes/(auth)/forget-password/+page.server.ts
@@ -24,7 +24,7 @@ export const actions: Actions = {
 
     // Password recovery logic here (using Supabase password reset email)
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: 'https://chat-bot-iota-eight.vercel.app/reset-password',
+      redirectTo: 'https://chat-bot-kappa-ruby.vercel.app/reset-password',
     });
 
     if (error) {
